781989281777ea741ca3ed4ed3480a025d7ce64e
dam2 - Díaz Vilas <a18alejandrodv@a13eql30.sanclemente.local>
2020-02-19 12:53:28 +0100

    Ejercicio Vistas casi acabado

--- Chapter06/r6_search_on_model/my_library/__manifest__.py
+++ Chapter06/r6_search_on_model/my_library/__manifest__.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 {
-    'name': "My Library",  # Module title
+    'name': "Chapter6_06",  # Module title
     'summary': "Manage books easily",  # Module subtitle phrase
     'description': """Long description""",  # You can also rst format
     'author': "Parth Gajjar",
@@ -12,7 +12,9 @@
     'data': [
         'security/groups.xml',
         'security/ir.model.access.csv',
+        'views/library_loan.xml',
         'views/library_book.xml',
+        'views/library_member.xml',
         'views/library_book_categ.xml'
     ],
     # This demo data files will be loaded if db initialize with demo data (commented becaues file is not added in this example)
